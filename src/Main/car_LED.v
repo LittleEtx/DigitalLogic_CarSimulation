d158d4642e214d132bb42ff9ca00019452a12439
Rosswasd <115915514+Rosswasd@users.noreply.github.com>
2023-01-04 23:26:43 +0800

    Add files via upload

--- src/Main/car_LED.v
+++ src/Main/car_LED.v
@@ -22,14 +22,39 @@
 
 module car_LED(
     input clk,
-    input [1:0] mode,
+    input[1:0] state,
     input turn_left,
     input turn_right,
-    output left_light,
-    output right_light
+    output reg left_light,
+    output reg right_light
     );
-
-//TODO
-assign left_light = 1'b1;
-assign right_light = 1'b1;
+parameter OFF = 2'b00,NOT_STARTING=2'b01,STARTING=2'b11,MOVING=2'b10;
+reg cnt=1'b0;
+always@(posedge clk or state)
+  begin
+  if(cnt==1'b0) begin
+  left_light=1'b0;
+  right_light=1'b0;
+  end
+  cnt=1'b1;
+  case(state)
+  OFF:
+    begin
+    left_light<=1'b0;
+    right_light<=1'b0;
+    end
+  NOT_STARTING:
+    begin
+    left_light<=1'b1;
+    right_light<=1'b1;
+    end
+  default begin
+    case({turn_left,turn_right})
+    2'b10: begin left_light<=~left_light;  right_light<=1'b0; end
+    2'b01: begin right_light<=~right_light; left_light<=1'b0; end
+    default begin right_light<=1'b0; left_light<=1'b0; end
+    endcase
+    end
+  endcase
+  end
 endmodule
